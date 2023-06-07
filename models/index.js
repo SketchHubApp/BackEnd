d433ab781d45fba22247789a6dce1980bceea012
Yunseo <kys051348@gmail.com>
2023-06-07 15:55:07 +0900

    Add Workspace DB Model init, associate
    + user-workspace n:m model
    workspace 모델 초기화 및 관계 설정

--- models/index.js
+++ models/index.js
@@ -3,6 +3,8 @@ const User = require('./user');
 const Profile = require('./profile');
 const Authentication = require('./authentication');
 const SocialLogin = require('./social_login');
+const Workspace = require('./workspace');
+const UserWorkspace = require('./userWorkspace');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
@@ -16,17 +18,23 @@ const db = {
     User,
     Profile,
     Authentication,
-    SocialLogin
+    SocialLogin,
+    Workspace, // Workspace 모델 추가
+    UserWorkspace
 };
 
 User.init(sequelize);
 Profile.init(sequelize);
 Authentication.init(sequelize);
 SocialLogin.init(sequelize);
+Workspace.init(sequelize); // Workspace 모델 초기화
+UserWorkspace.init(sequelize);
 
 User.associate(db);
 Profile.associate(db);
 Authentication.associate(db);
 SocialLogin.associate(db);
+Workspace.associate(db); // Workspace 모델 관계 설정
+UserWorkspace.associate(db)
 
 module.exports = db;
